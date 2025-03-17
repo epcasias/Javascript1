9266a8421018c75df13fdb9b183d91be09fe119f
Eulyssia Casias <epcasias@amarillocollege.com>
2025-03-17 17:03:58 -0500

    comment

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    // display alert pop up good afternoon
+    window.alert("Good afternoon");
+    // declare the variable to hold the div element
+    let container = document.getElementById("container");
+    // set the content of element
+    container.innerHTML = "This is the container place";
+    //change the tetx color to red
+    container.style.color = "red";
+    // add more text to page
+    document.write("<p>This is a new paragraph</p>")
